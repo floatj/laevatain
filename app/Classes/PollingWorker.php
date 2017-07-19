2f905135aac2e15c665eb831811b03c507abbfa2
johnny <johnny_liao@johnny-liao-rd2>
2017-07-19 21:01:29 +0800

    add telegram lib

--- app/Classes/PollingWorker.php
+++ app/Classes/PollingWorker.php
@@ -6,18 +6,20 @@ namespace App\Classes;
  */
 
 use App\Api\Telegram as TelegramApi;
-
+use App\Lib\TelegramLib as TelegramLib;
 
 Class PollingWorker
 {
 
     protected $timeout = 120;   //LongPolling 的 Timeout，目前設超過 50 秒似乎對 Telegram 官方 API 沒生效
     public $_api;               //API物件
+    public $_lib;               //Lib
     public $offset;             //要回傳給 GetUpdates Api 的 Offset
 
     function __construct()
     {
         $this->_api = new TelegramApi();
+        $this->_lib = new TelegramLib();
         $this->offset = 0;
 
         echo "- telegram api 初始化完成\n";
@@ -29,15 +31,19 @@ Class PollingWorker
     function run()
     {
         //使用 long polling
-        while(true) {
+        while(true)
+        {
 
             //如果有回傳新的 offset 就把回傳的新的 offset 帶入參數
-            //echo "new_offset = ".$new_offset."\n";
-            //echo (!empty($new_offset) AND $new_offset != 0 )."\n";
             echo "\n\033[1;30m[" . date('H:i:s') . "]\033[m 送出 getUpdates API Request (long polling mode) ...\n";
 
             $ret = $this->_api->getUpdates($this->timeout, (!empty($new_offset) AND $new_offset != 0) ? $new_offset + 1 : $this->offset);
-            $new_offset = $this->handleRet($ret);
+
+            //取得最新的 offset
+            $new_offset = $this->_lib->getLatestOffset($ret);
+
+            //處理回應
+            $this->handleUpdates($ret);
         }
 
     }
@@ -46,16 +52,18 @@ Class PollingWorker
      * 處理 Updates (PollingWorker 物件版本)
      *
      * @param $ret getUpdates API 回傳結果 (一大串 updates)
-     * @return int $last_update_id 或 0
+     * @return int 處理結果（？）
      */
-    function handleRet($ret){
+    function handleUpdates($ret)
+    {
 
-        echo "觸發 handle Ret!!\n";
-        var_dump($ret);
+        echo "觸發 handle Updates!!\n";
+
+        //var_dump($ret);
+        //@todo 此處插入 fork 機制
         
         //@todo 把 lib/lib.php 內的 function refactor 並以適當方式整合進 laravel
-        //$last_update_id = updatesHandler($ret);
-        //return $last_update_id;
+        $this->_lib->processUpdates($ret['result']);
     }
 
 
---
+++ app/Lib/TelegramLib.php
@@ -0,0 +1,102 @@
+<?php
+namespace App\Lib;
+
+use App\Api\Telegram as TelegramApi;
+
+
+//@todo 思考一下這些到底該放哪邊??? controller???
+/* 一些輔助用的 Lib */
+class TelegramLib
+{
+
+    /**
+     * 取得最新一筆的 offset
+     */
+    public function getLatestOffset($ret)
+    {
+        //若 api 有回傳結果，且 result 不為空，則取最後一筆 result 的 update_id
+        return ($ret['ok'] == true AND empty($ret['result'])) ? 0 : end($ret['result'])['update_id'];
+    }
+
+    /**
+     * 處理 Updates 分割為單一訊息，並將每一則訊息傳入 processSingleMsg 作處理
+     * @param $updates  API 回傳的 updates 陣列
+     */
+    function processUpdates($updates)
+    {
+
+
+        echo "\n\n\033[1;33m收到的 Updates 數量共有 ".count($updates)." 則，逐一處理: \033[m\n";
+
+        foreach($updates as $msg)
+        {
+
+            //處理每一則訊息
+            $this->processSingleMsg($msg);
+        }
+
+        echo "\n\033[1;33m所有 Updates 處理完成 \033[m\n\n";
+    }
+
+    /**
+     * 處理單一訊息
+     *
+     * @param $msg
+     */
+    function processSingleMsg($msg)
+    {
+
+        echo "\n\033[1;30m處理單一訊息:\033[m\n";
+        echo "-------------------------------\n";
+        echo "update id = " . $msg['update_id'] . "\n";
+        echo "user_id   = " . $msg['message']['chat']['id'] . "\n";
+        echo "username  = " . $msg['message']['chat']['username'] . "\n";
+        echo "msg       = " . $msg['message']['text'] . "\n";
+        echo "-------------------------------\n";
+
+        //處理這則訊息 (回應使用者, 不回應使用者... or 其他可指定的動作)
+        $this->engageSingleMsg($msg['message']['chat']['id'], $msg['message']['text']);
+
+    }
+
+    /**
+     * 處理單一訊息 bot 行為
+     * @param $user_id  發送訊息的使用者 id
+     * @param $text     發送訊息的內容
+     */
+    function engageSingleMsg($user_id, $text)
+    {
+        //(debug) 測試用 -- 延遲一秒
+        //sleep(1);
+
+        //@todo 加入更多可選擇的處理動作
+        $this->echoSingleMsg($user_id, $text, "你好，你剛剛說的話是: ");
+    }
+
+    /**
+     * 回應使用者訊息
+     * @param $user_id
+     * @param $text
+     */
+    function echoSingleMsg($user_id, $text, $prefix="")
+    {
+
+        //@todo 這邊設計顯然有問題，classes -> lib -> api ???
+        //@todo 需要 refactor 並思考放置位置
+
+        $tg = new TelegramApi();
+        $ret = $tg->sendMsg($user_id, $prefix . $text);
+
+        //API 回傳值是 json，需要 decode 再使用，否則會出現錯誤
+        $ret = json_decode($ret,true);
+
+        if ($ret['ok'] == true) {
+            echo "發送訊息成功\n";
+            //print_r($ret);
+        } else {
+            echo "\033[1;31m發送訊息失敗，請檢查設定檔或網路狀況\033[m\n";
+        }
+    }
+
+
+}
